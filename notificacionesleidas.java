92c29399e65e4497c40ad09419118fd31947f801
samuelrobledo52 <132373559+samuelrobledo52@users.noreply.github.com>
2024-09-18 23:27:55 -0600

    Delete notificacionesleidas.java

--- notificacionesleidas.java
+++
@@ -1,4 +0,0 @@
-public void markNotificationAsRead(User user, String notificationId) {
-        List<Notification> notifications = userNotifications.get(user);
-
-// Sistema de marcaje de notificaciones 
