4d9b629970f8244d1670f1518f48286a87837e58
DiegoD762 <diegoantoniodeleone@gmail.com>
2024-09-18 22:57:59 -0600

    Update MatchingSystem.java
    cree instancia de matchingsystem

--- MatchingSystem.java
+++ MatchingSystem.java
@@ -4,3 +4,10 @@ import java.util.stream.Collectors;
 public class MatchingSystem {
   private List<Student> students;
   private List<Tutor> tutors;
+
+  public Marching System() {
+    this.students = new ArrayList<>();
+    this.tutors = new ArrayList<>();
+  }
+
+  
