757879a1f8d4795ab83267502b6efafcf1a06e1e
samuelrobledo52 <132373559+samuelrobledo52@users.noreply.github.com>
2024-09-18 23:28:17 -0600

    Delete classnoti.java

--- classnoti.java
+++
@@ -1,8 +0,0 @@
-public class Notification {
-    private String id;
-    private User recipient;
-    private String message;
-    private LocalDateTime timestamp;
-    private boolean isRead;
-
-// clase para funcionamiento del archivo 
