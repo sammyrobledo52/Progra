eddc531aae65bc30ceaead8ba2791104c06591d9
San231926 <san231926@uvg.edu.gt>
2024-09-18 23:02:33 -0600

    Crear clase Schedule

---
+++ Schedule.java
@@ -0,0 +1,16 @@
+/**
+ * Clase que representa un horario de disponibilidad.
+ * Permite agregar y eliminar franjas horarias disponibles para un tutor o estudiante.
+ */
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Schedule {
+    // Lista que almacena las franjas horarias disponibles.
+    private List<TimeSlot> availableTimes;
+
+
+
+}
