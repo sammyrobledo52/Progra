d86da8c0bb68354d406ccc5dc5fef68cde0a5bc0
samuelrobledo52 <132373559+samuelrobledo52@users.noreply.github.com>
2024-09-18 23:34:04 -0600

    Update notificacion.java

--- notificacion.java
+++ notificacion.java
@@ -79,5 +79,12 @@ public class Notification {
     private boolean isRead;
 // clase para funcionamiento del archivo 
 
-
+public Notification(User recipient, String message) {
+        this.id = UUID.randomUUID().toString();
+        this.recipient = recipient;
+        this.message = message;
+        this.timestamp = LocalDateTime.now();
+        this.isRead = false;
+    }
+// base de donde parte la notificacion con sus atributos 
 
