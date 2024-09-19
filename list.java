e5d46fb563f8da5bf3c1a49c6b4dfc969a88db31
samuelrobledo52 <132373559+samuelrobledo52@users.noreply.github.com>
2024-09-18 23:28:29 -0600

    Delete list.java

--- list.java
+++
@@ -1,5 +0,0 @@
-public List<Notification> getNotificationsForUser(User user) {
-        return userNotifications.getOrDefault(user, new ArrayList<>());
-    }
-
-//Listado de las notificaciones con sus constructores
