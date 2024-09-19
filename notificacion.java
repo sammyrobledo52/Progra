b0d9f4c611a69d368da4863f14345c75cc444f31
samuelrobledo52 <132373559+samuelrobledo52@users.noreply.github.com>
2024-09-18 23:31:01 -0600

    Update notificacion.java

--- notificacion.java
+++ notificacion.java
@@ -35,3 +35,14 @@ public class NotificationSystem {
     private Map<User, List<Notification>> userNotifications;
   // Listado notificaciones usar los atributos
 
+
+ public NotificationSystem() {
+        this.userNotifications = new HashMap<>();
+    }
+    public void sendNotification(User recipient, String message) {
+        Notification notification = new Notification(recipient, message);
+        userNotifications.computeIfAbsent(recipient, k -> new ArrayList<>()).add(notification);
+    }
+// Crear el sistema de notificaciones usando una funcion 
+
+
