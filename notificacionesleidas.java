6fafd0b4909e18f527b2b61d4f165182fa6c2138
samuelrobledo52 <132373559+samuelrobledo52@users.noreply.github.com>
2024-09-18 23:14:04 -0600

    Create notificacionesleidas.java

---
+++ notificacionesleidas.java
@@ -0,0 +1,4 @@
+public void markNotificationAsRead(User user, String notificationId) {
+        List<Notification> notifications = userNotifications.get(user);
+
+// Sistema de marcaje de notificaciones 
