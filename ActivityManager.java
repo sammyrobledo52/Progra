9ff736581f4bd8f16f51ed3e2e62e12ab7f29139
ignacioaris <arisignacio90@gmail.com>
2024-09-18 22:54:22 -0600

    definir atributos

--- ActivityManager.java
+++ ActivityManager.java
@@ -3,5 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ActivityManager{
-    
+
+    private List<AcademicActivity> activities;
+    private NotificationSystem notificationSystem;
 }
