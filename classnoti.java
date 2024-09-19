db1c623a227217223eac187d6990258a68fd736f
samuelrobledo52 <132373559+samuelrobledo52@users.noreply.github.com>
2024-09-18 23:15:30 -0600

    Create classnoti.java

---
+++ classnoti.java
@@ -0,0 +1,8 @@
+public class Notification {
+    private String id;
+    private User recipient;
+    private String message;
+    private LocalDateTime timestamp;
+    private boolean isRead;
+
+// clase para funcionamiento del archivo 
