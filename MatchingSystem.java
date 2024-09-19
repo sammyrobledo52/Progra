75a22442b52957e2c5446a0495a6831f5492e1be
DiegoD762 <diegoantoniodeleone@gmail.com>
2024-09-18 23:04:17 -0600

    Update MatchingSystem.java
    cree metodo que busca compatibilidad

--- MatchingSystem.java
+++ MatchingSystem.java
@@ -15,6 +15,13 @@ public class MatchingSystem {
   public void addTutor(Tutor tutor) {
     tutors.add(tutor);
   }
+  public List<Tutor> findMatchesForStudent(Student student) {
+    return tutors.stream()
+            .filter(tutor -> hasCommonInterests(student, tutor))
+            .filter(this::isAvailable)
+            .sorted(Comparator.comparingDouble(tutor -> calculateMatchScore(student, tutor)))
+            .collect(Collectors.toList());
+  }
   
 
   
