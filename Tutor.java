7e1c07c873f94bb9430b7b7b3e8619f450205d67
ignacioaris <arisignacio90@gmail.com>
2024-10-14 14:23:50 -0600

    agregar parametros y metodos a  Tutor.java

--- Tutor.java
+++ Tutor.java
@@ -1,10 +1,15 @@
+
 public class Tutor extends User {
+    private Schedule availability;
     private String subjectExpertise; // Materia en la que es experto el tutor
+    private float rating;
 
     // Constructor
     public Tutor(String id, String name, String email, String password, String subjectExpertise) {
         super(id, name, email, password);
+        this.availability = new Schedule();
         this.subjectExpertise = subjectExpertise;
+        this.rating = 0;
     }
 
     // Getter y Setter para 'subjectExpertise'
@@ -20,5 +25,8 @@ public class Tutor extends User {
     public void displayInfo() {
         super.displayInfo();
         System.out.println("Subject Expertise: " + subjectExpertise);
+    }
+    public Schedule getAvailability() {
+        return availability;
     }
 }
