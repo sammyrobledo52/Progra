01ed3402c4f5b4c9227e6a13256a5f0705637fe5
ignacioaris <arisignacio90@gmail.com>
2024-10-15 16:40:47 -0600

    añadir parámetros métodos y corregir el @override Tutor.java

--- Tutor.java
+++ Tutor.java
@@ -1,13 +1,15 @@
 
 public class Tutor extends User {
     private Schedule availability;
+    private List<String> specializations;
     private String subjectExpertise; // Materia en la que es experto el tutor
     private float rating;
 
     // Constructor
-    public Tutor(String id, String name, String email, String password, String subjectExpertise) {
+    public Tutor(String id, String name, String email, String password, List<String> specializations,String subjectExpertise) {
         super(id, name, email, password);
         this.availability = new Schedule();
+        this.specializations = specializations; 
         this.subjectExpertise = subjectExpertise;
         this.rating = 0;
     }
@@ -21,11 +23,18 @@ public class Tutor extends User {
         this.subjectExpertise = subjectExpertise;
     }
 
+    public List<String> getSpecializations() {
+        return specializations;
+    }
+
     @Override
     public void displayInfo() {
         super.displayInfo();
         System.out.println("Subject Expertise: " + subjectExpertise);
+        System.out.println("Specializations: " + String.join(", ", specializations));
+        System.out.println("Rating: " + rating);
     }
+    
     public Schedule getAvailability() {
         return availability;
     }
