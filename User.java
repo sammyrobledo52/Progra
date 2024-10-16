93920788112233d4a7e3903c5a3de412cb8cf571
DiegoD762 <diegoantoniodeleone@gmail.com>
2024-10-15 18:28:49 -0600

    Update User.java
    Agregué métodos

--- User.java
+++ User.java
@@ -11,4 +11,29 @@ public class User {
     this.password = password;
   }
 
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+  
+
   
