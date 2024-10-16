eb08c9a97c62295d16658c8ed622b4a5e79e0bcf
ignacioaris <arisignacio90@gmail.com>
2024-10-15 18:13:37 -0600

    añadir atributos y constructor a TimeSlot.java

--- TimeSlot.java
+++ TimeSlot.java
@@ -1,5 +1,14 @@
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 public class TimeSlot{
+  
+  private DayOfWeek day;
+  private LocalTime startTime;
+  private LocalTime endTime;
 
+  public TimeSlot(DayOfWeek day, LocalTime startTime, LocalTime endTime) {
+      this.day = day;
+      this.startTime = startTime;
+      this.endTime = endTime;
+    }
 }
