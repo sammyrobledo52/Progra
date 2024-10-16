16d090f8103e56db39907160daf9da0793b62d60
acorado1234 <146757763+acorado1234@users.noreply.github.com>
2024-10-15 21:25:41 -0600

    eliminar librerías sin usar Schedule.java

--- Schedule.java
+++ Schedule.java
@@ -2,8 +2,7 @@
  * Clase que representa un horario de disponibilidad.
  * Permite agregar y eliminar franjas horarias disponibles para un tutor o estudiante.
  */
-import java.time.DayOfWeek;
-import java.time.LocalTime;
+
 import java.util.ArrayList;
 import java.util.List;
 
