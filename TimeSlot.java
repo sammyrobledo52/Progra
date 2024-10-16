a7b3d386b72f32cfafc3dd09c82b7c72ffa6504d
ignacioaris <arisignacio90@gmail.com>
2024-10-15 18:20:11 -0600

    añadir getters y setters en TimeSlot.java

--- TimeSlot.java
+++ TimeSlot.java
@@ -11,4 +11,28 @@ public class TimeSlot{
       this.startTime = startTime;
       this.endTime = endTime;
     }
+
+  public DayOfWeek getDay() {
+        return day;
+    }
+
+    public void setDay(DayOfWeek day) {
+        this.day = day;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
 }
