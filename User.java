c16698391abc3928ad4d76903e22e289c97430db
DiegoD762 <diegoantoniodeleone@gmail.com>
2024-10-15 18:24:00 -0600

    Update User.java
    agregue un constructor para inicializar atributos previamente hechos

--- User.java
+++ User.java
@@ -3,3 +3,12 @@ public class User {
   private String name;
   private String email;
   private String password;
+
+  public User(String id, String name, String email, String password) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
+
+  
