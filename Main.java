808f54f4e00d4a6ca43fdb1362ea996aca12f5f4
San231926 <san231926@uvg.edu.gt>
2024-10-15 18:39:49 -0600

    Se crearon listas de User y de academicActivity

--- Main.java
+++ Main.java
@@ -1,7 +1,24 @@
-public class Main{
+public class Main {
 
- public static void main(String[] args){
+  /**
+   * Lista de usuarios que participan en el sistema.
+   * Contiene instancias de la clase {@link User}, que puede incluir tanto estudiantes como tutores.
+   */
+    private static ArrayList<User> users = new ArrayList<>();
+   /**
+    * Lista de actividades académicas disponibles en el sistema.
+    * Contiene instancias de la clase {@link AcademicActivity}, que representan diferentes actividades académicas.
+    */
+    private static ArrayList<AcademicActivity> activities = new ArrayList<>();
+    private static Scanner scanner = new Scanner(System.in);
+
+   /**
+    * Objeto {@link Scanner} utilizado para leer entradas desde la consola.
+    * Se utiliza en varios métodos para recibir entradas del usuario.
+    */
+    public static void main(String[] args) {
+
+     }
 
 
- }
 }
